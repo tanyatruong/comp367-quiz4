d5ce3f2e247eb4ac58b45cf517eda49dfa419955
Tanya Truong <mtruon32@my.centennialcollege.ca>
2025-04-02 10:53:24 -0400

    Initial commit: Created ASP.NET Web App

---
+++ COMP367Quiz4WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP367Quiz4WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
